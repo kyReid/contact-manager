8b3884972bb6901590577539adb5ab5e8357789f
Jorge B. Nunez <jjbnunez97@gmail.com>
2019-01-22 17:40:54 -0500

    File system restructuring to accomodate Angular components. Will require proper integration later.

---
+++ src/app/contactlist/contactlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactlist',
+  templateUrl: './contactlist.component.html',
+  styleUrls: ['./contactlist.component.css']
+})
+export class ContactlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
