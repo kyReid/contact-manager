bffc3b7c23acc58fc8cb4f26fca7523913f5fe01
unknown <kyreid14@gmail.com>
2019-01-23 16:14:55 -0500

    Add existing file

---
+++ ContactManager/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactlistComponent } from './contactlist/contactlist.component';
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'contactlist', component: ContactlistComponent}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
