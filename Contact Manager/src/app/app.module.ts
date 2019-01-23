f4e899dea489ac990809b42c00d389799fb8ed25
Kyle Reid <40274419+kyReid@users.noreply.github.com>
2019-01-23 15:50:58 -0500

    Update app.module.ts

--- Contact Manager/src/app/app.module.ts
+++ Contact Manager/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
