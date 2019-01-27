5ab3833342ab41a8d88408d07bc1e74ae2b1e277
chbshoot <jmluismoreno@gmail.com>
2019-01-27 15:37:56 -0500

    create contact component frontend stuff, hacky push onto authService's user's contact array

--- contact_man/frontend-src/src/app/components/create-contact/create-contact.component.ts
+++ contact_man/frontend-src/src/app/components/create-contact/create-contact.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { AuthService } from '../../services/auth.service';
+import { Contact } from '../../entities';
+
 @Component({
   selector: 'app-create-contact',
   templateUrl: './create-contact.component.html',
   styleUrls: ['./create-contact.component.css']
 })
 export class CreateContactComponent implements OnInit {
+  newContact: Contact = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onCreateContactSubmit() {
+    if (!this.authService.user.contacts) {
+      this.authService.user.contacts = [];
+    }
+
+    this.authService.user.contacts.push(this.newContact);
+  }
 }
